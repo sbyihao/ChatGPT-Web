e90215d8c58a1c43390bbc1406fda65055318aa4
95i <54938994+95i@users.noreply.github.com>
2023-05-02 16:14:07 +0800

    Update zh-CN.ts

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -55,7 +55,7 @@ export default {
     general: '总览',
     advanced: '高级',
     config: '配置',
-    avatarLink: '头像链接1111',
+    avatarLink: '头像链接',
     name: '名称',
     description: '描述',
     role: '角色设定',
